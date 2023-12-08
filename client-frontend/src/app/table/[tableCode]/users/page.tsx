a778a46ccdc29ad0fbb7d93421acb31c1429d0ea
edmaia <42702623+edmaia97@users.noreply.github.com>
2023-12-08 10:29:18 -0300

    Update page.tsx

--- client-frontend/src/app/table/[tableCode]/users/page.tsx
+++ client-frontend/src/app/table/[tableCode]/users/page.tsx
@@ -23,16 +23,17 @@ export default function UsersPage() {
 
   const [tableUsers, setTableUsers] = useState([]);
 
-  const uri = backendURL + '/table/' + session?.table_number + '/stream/';
-  const ssEvents = new EventSource(
-    uri, { withCredentials: false }
-  );
+  // const uri = backendURL + '/table/' + session?.table_number + '/stream/';
+  // const ssEvents = new EventSource(
+  //   uri, { withCredentials: false }
+  // );
 
   useEffect(() => {
     const getFetchUsers = async () => {
-      const user_session = await getSession();
       try {
-        const res = await getAllUsers(user_session?.user?.accessToken);
+        const user_session = await getSession();
+        const accessToken = user_session.user.accessToken || "";
+        const res = await getAllUsers(accessToken);
         const { error } = res;
 
         if (error) {
@@ -50,56 +51,56 @@ export default function UsersPage() {
     getFetchUsers();
   }, []);
 
-  useEffect(() => {
-    // listen to login event
-    ssEvents.addEventListener("join-table", (e:any) => {
-      const user = JSON.parse(e.data);
+  // useEffect(() => {
+  //   // listen to login event
+  //   ssEvents.addEventListener("join-table", (e:any) => {
+  //     const user = JSON.parse(e.data);
 
-      if(!(user.username === session?.user?.name)) {
-        toast("A new user joined the table!", {
-          position: "bottom-right",
-          autoClose: 2000,
-          draggable: true,
-          pauseOnHover: true,
-          progress: undefined,
-          hideProgressBar: false,
-          toastId: 'join',
-        });
-        setTableUsers((prevUsers) => [...prevUsers, user]);
-      }
-    });
+  //     if(!(user.username === session?.user?.name)) {
+  //       toast("A new user joined the table!", {
+  //         position: "bottom-right",
+  //         autoClose: 2000,
+  //         draggable: true,
+  //         pauseOnHover: true,
+  //         progress: undefined,
+  //         hideProgressBar: false,
+  //         toastId: 'join',
+  //       });
+  //       setTableUsers((prevUsers) => [...prevUsers, user]);
+  //     }
+  //   });
 
-    ssEvents.addEventListener("leave-table", (e:any) => {
-      const user = JSON.parse(e.data);
-      const message =
-        user.username === session?.user?.name
-          ? "Bye!"
-          : "A user left the table!";
-      toast(message, {
-        position: "bottom-right",
-        autoClose: 2000,
-        draggable: true,
-        pauseOnHover: true,
-        progress: undefined,
-        hideProgressBar: false,
-        toastId: 'leave',
-      });
-      setTableUsers(prevUsers => prevUsers.filter(item => item.id !== user.id));
-    });
+  //   ssEvents.addEventListener("leave-table", (e:any) => {
+  //     const user = JSON.parse(e.data);
+  //     const message =
+  //       user.username === session?.user?.name
+  //         ? "Bye!"
+  //         : "A user left the table!";
+  //     toast(message, {
+  //       position: "bottom-right",
+  //       autoClose: 2000,
+  //       draggable: true,
+  //       pauseOnHover: true,
+  //       progress: undefined,
+  //       hideProgressBar: false,
+  //       toastId: 'leave',
+  //     });
+  //     setTableUsers(prevUsers => prevUsers.filter(item => item.id !== user.id));
+  //   });
 
-    // listen to open event
-    ssEvents.onopen = (e:any) => {
-      console.log(e);
-    };
-    // listen to error event
-    ssEvents.onerror = (e:any) => {
-      console.log(e);
-    };
+  //   // listen to open event
+  //   ssEvents.onopen = (e:any) => {
+  //     console.log(e);
+  //   };
+  //   // listen to error event
+  //   ssEvents.onerror = (e:any) => {
+  //     console.log(e);
+  //   };
 
-    return () => {
-      ssEvents.close();
-    };
-  }, []);
+  //   return () => {
+  //     ssEvents.close();
+  //   };
+  // }, []);
 
   return (
       <div className='h-screen text-white'>
