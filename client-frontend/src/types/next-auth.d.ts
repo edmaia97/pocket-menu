afd83b99c4dfcaea53a3312375c95580ec88617b
edmaia <42702623+edmaia97@users.noreply.github.com>
2023-12-08 09:42:11 -0300

    Update next-auth.d.ts

--- client-frontend/src/types/next-auth.d.ts
+++ client-frontend/src/types/next-auth.d.ts
@@ -1,10 +1,16 @@
-import NextAuth from 'next-auth'
+import NextAuth from 'next-auth';
+import type { DefaultSession } from 'next-auth';
 
 declare module 'next-auth' {
     /**
      * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
      */
     interface Session {
+        user: DefaultSession['user'] & {
+            id: string;
+            name: string;
+            accessToken: string;
+        };
         restaurant_id: string | null,
         restaurant_name: string | null,
         table_number: string | null,
