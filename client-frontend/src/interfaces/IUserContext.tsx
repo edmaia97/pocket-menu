6545e84ae5bb5bdabaec41dcf94ffaaf135fcfb4
edmaia <42702623+edmaia97@users.noreply.github.com>
2023-12-08 12:26:08 -0300

    Update IUserContext.tsx

--- client-frontend/src/interfaces/IUserContext.tsx
+++ client-frontend/src/interfaces/IUserContext.tsx
@@ -1,6 +1,6 @@
 export default interface IUserContext {
     username: string | React.Dispatch<React.SetStateAction<string>>;
-    setUsername: React.Dispatch<React.SetStateAction<string>>;
+    setUsername: string | React.Dispatch<React.SetStateAction<string>>;
     token: string | React.Dispatch<React.SetStateAction<string>>;
-    setToken: React.Dispatch<React.SetStateAction<string>>;
+    setToken: string | React.Dispatch<React.SetStateAction<string>>;
 }
